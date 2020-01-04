b4addab1e7f964ab8c07454458e34864f2ddeece
gabin.bloquet <gabin.bloquet@decathlon.com>
2020-01-04 11:44:51 +0100

    :rocket: Styling app, connect form, CSS variables

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,69 @@
 <script>
-	export let name;
+	export let lastname;
+	export let firstname;
+
+	function handleClick() {
+		console.log(lastname, firstname)
+	}
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Welcome on Svelte covoit'</h1>
+	<form on:submit|preventDefault>
+		<h2>Identifiez vous</h2>
+		<label>
+			<p>Nom</p>
+			<input bind:value={lastname}>
+		</label>
+		<label>
+			<p>Prenom</p>
+			<input bind:value={firstname}>
+		</label>
+		<button on:click|once={handleClick}>
+			Log in
+		</button>
+	</form>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
 	h1 {
-		color: #ff3e00;
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
+		color: var(--COLOR-PINK);
 	}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	h2 {
+		color: var(--COLOR-BROWN)
+	}
+
+	form {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		padding: 2em;
+		background-color: var(--COLOR-DARK-ORANGE);
+	}
+
+	label {
+		display: flex;
+		flex-direction: column;
+		font-size: medium;
+	}
+
+	input {
+		font-size: large;
+		margin-bottom: 1em;
+	}
+
+	p {
+		margin-bottom: 0.5em;
+	}
+
+	button {
+		font-size: large;
+		padding: 0.5em;
+		color: white;
+		background-color: var(--COLOR-BROWN);
 	}
 </style>
