b23200ad0ee13401b8ed0cb90d5c3576a8a5fb46
gabin.bloquet <gabin.bloquet@decathlon.com>
2020-01-09 10:18:18 +0100

    :sparkles: connexion with local database

--- src/utils/services/user/userService.js
+++ src/utils/services/user/userService.js
@@ -1,4 +1,14 @@
 function getUser(lastname, firstname){
+  let users = [];
+  fetch('../static/database.json')
+    .then(response => response.json())
+    .then(data => users = data)
+
+  const user = users
+    .map(user => user.lastname === lastname && user.firstname === firstname);
+
+  console.log(user);
+
   return {
     lastname: lastname,
     firstname: firstname
@@ -17,3 +27,4 @@ function saveUserTravel(user, travel) {
 }
 
 export {getUser, loadUserDailyTravel, saveUserTravel};
+
